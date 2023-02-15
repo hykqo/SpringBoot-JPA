f2a13afd039dd611c539305d77b24a1db2845235
ENOCH YEOM <hykqo@naver.com>
2023-02-15 21:17:06 +0900

    view 환경 셋팅 및 mav예제

---
+++ jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    //Model에 싫어서 뷰로 데이터를 넘길 수 있다.
+    @GetMapping("hello")
+    public String hello(Model model){
+        //data라는 키에 hello!!라는 글자를 담아서 넘긴다.
+        model.addAttribute("data","hello!!");
+        //view네임을 명시한다
+        return "hello";
+
+    }
+}
