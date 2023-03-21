6e8c03da4ae04ee4d50c554ebb5dff5f69d58f56
ENOCH YEOM <hykqo@naver.com>
2023-03-21 09:36:02 +0900

    동적쿼리 작성 - String 이용, Criteria이용

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -3,8 +3,12 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -20,5 +24,71 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+
+    /**
+    *JPQL 쿼리를 문자로 생성하기는 번거롭고, 실수로 인한 버그가 충분히 발생할 수 있다.
+    */
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class) .setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+    }
+
+    /**
+     * JPA Criteria = 자바로 JPQL을 직접 만들수 있도록 해주는 도구
+     * 그러나 실무에서는 안쓰인다.
+     * 실무에서는 sql만 보고 바로 어떤 데이터가 나올지 예측이 되어야 하는데, criteria는 너무 난해해서, 실무에서 사용한다면 유지보수에 매우 힘들다.
+     * */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> from = cq.from(Order.class);
+        Join<Object, Object> member = from.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+        //회원 상태 검색
+        if(orderSearch.getOrderStatus() != null){
+            Predicate status = cb.equal(from.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(from.get("name"), "%"+orderSearch.getMemberName()+"%");
+            criteria.add(name);
+        }
+
+        cq.where(criteria.toArray(new Predicate[criteria.size()]));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+
+    }
 }
