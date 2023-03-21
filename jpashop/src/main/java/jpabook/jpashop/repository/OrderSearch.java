f84bd4463455d0e74bf6f95645b364ee39ef4bb6
ENOCH YEOM <hykqo@naver.com>
2023-03-21 09:35:38 +0900

    orderSearch DTO 셋팅

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
