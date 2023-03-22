72f248c9003794b0140c767e190106e7a8ed9475
ENOCH YEOM <hykqo@naver.com>
2023-03-22 09:26:20 +0900

    상품 등록기능 작성

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookController.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("items")
+public class BookController {
+
+    private final ItemService itemService;
+
+    @GetMapping("new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("new")
+    public String create(BookForm form){
+        Book book = Book.createBook(form.getName(), form.getPrice(), form.getStockQuantity(), form.getAuthor(), form.getIsbn());
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+}
+
+
