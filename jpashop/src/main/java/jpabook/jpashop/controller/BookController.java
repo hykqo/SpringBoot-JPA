5a962cbdd874ba7f1e64fe24a19a1132c9b6b804
ENOCH YEOM <hykqo@naver.com>
2023-03-22 09:31:43 +0900

    상품 목록페이지/기능 작성

--- jpashop/src/main/java/jpabook/jpashop/controller/BookController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookController.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,6 +30,13 @@ public class BookController {
         Book book = Book.createBook(form.getName(), form.getPrice(), form.getStockQuantity(), form.getAuthor(), form.getIsbn());
         itemService.saveItem(book);
         return "redirect:/items";
+    }
+
+    @GetMapping("")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
 
